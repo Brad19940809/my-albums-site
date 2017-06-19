bb8ada584ba9398f18fd0e7b7e67b5c62d533272
Brad <xiaoleluo2@gmail.com>
2017-06-19 10:48:09 +0800

    Modify the templates

--- music/static/lib/js/base.js
+++ music/static/lib/js/base.js
@@ -1,4 +1,9 @@
 /**
  * Created by Brad on 2017/6/18.
  */
-$()
+function p1Function() {
+    var p1 = document.getElementById("p1");
+    p1.style.color = "#ff0000";
+}
+
+document.getElementById("p1").innerHTML='have changed this line.'
